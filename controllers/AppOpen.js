f69bec4e709ead2c6dca8d28bc35ca0b7d9d5772
vicky chaudhary <vickychaudhary8955@gmail.com>
2024-06-03 14:09:20 +0530

    updated

--- controllers/AppOpen.js
+++ controllers/AppOpen.js
@@ -2,16 +2,13 @@ const AppOpen = require('../models/AppOpen');
 
 async function incrementAppOpen(req, res){
   try {
-    let appOpen = await AppOpen.findOne();
+    const appOpen = await AppOpen.findOneAndUpdate(
+      {},
+      { $inc: { count: 1 } },
+      { new: true, upsert: true, setDefaultsOnInsert: true }
+    );
 
-    if (!appOpen) {
-      appOpen = new AppOpen({ count: 1 });
-    } else {
-      appOpen.count += 1;
-    }
-
-    await appOpen.save();
-    res.status(200).json({ count: appOpen.count});
+    res.status(200).json({ count: appOpen.count });
   } catch (error) {
     res.status(500).json({ message: 'Server Error', error });
   }
