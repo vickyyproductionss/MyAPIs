7060ddeb30c29c3ef0dd42f8e3c3dff1c6a11583
vicky chaudhary <vickychaudhary8955@gmail.com>
2024-10-12 14:15:54 +0530

    Update Payment.js

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -38,8 +38,8 @@ async function CreatePaymentLink(req, res) {
             notes: {
                 policy_name: "Jeevan Bima"
             },
-            //callback_url: "https://my-ap-is-theta.vercel.app/api/payments/verification",
-            //callback_method: "post"
+            callback_url: "https://my-ap-isasdasd-theta.vercel.app/api/payments/verification",
+            callback_method: "get"
         };
 
         // Create the payment link
